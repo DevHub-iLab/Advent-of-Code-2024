00d6e03403d0efdf637be75b08e24fdaaf1585ea
niclee1803 <niclee1803@outlook.com>
2024-12-04 13:54:33 +0800

    day4

---
+++ niclee1803/day4/Day4pt2.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class Day4pt2 {
+    static boolean checkXMasShape(char[][] grid, int i, int j) {
+        // Center must be 'A'
+        if (grid[i][j] != 'A') return false;
+
+
+        // M - S
+        // - A -
+        // M - S
+        if (i > 0 && j > 0 && i < grid.length - 1 && j < grid[0].length - 1) {
+            if (grid[i-1][j-1] == 'M' && grid[i+1][j+1] == 'S' &&
+                    grid[i-1][j+1] == 'S' && grid[i+1][j-1] == 'M') {
+                return true;
+            }
+        }
+
+        // S - S
+        // - A -
+        // M - M
+        if (i > 0 && j < grid[0].length - 1 && i < grid.length - 1 && j > 0) {
+            if (grid[i-1][j-1] == 'S' && grid[i+1][j+1] == 'M' &&
+                    grid[i-1][j+1] == 'S' && grid[i+1][j-1] == 'M') {
+                return true;
+            }
+        }
+
+        // M - M
+        // - A -
+        // S - S
+        if (i < grid.length - 1 && j < grid[0].length - 1 && i > 0 && j > 0) {
+            if (grid[i-1][j-1] == 'M' && grid[i+1][j+1] == 'S' &&
+                    grid[i-1][j+1] == 'M' && grid[i+1][j-1] == 'S') {
+                return true;
+            }
+        }
+
+        // S - M
+        // - A -
+        // s - M
+        if (i < grid.length - 1 && j > 0 && i > 0 && j < grid[0].length - 1) {
+            if (grid[i-1][j-1] == 'S' && grid[i+1][j+1] == 'M' &&
+                    grid[i-1][j+1] == 'M' && grid[i+1][j-1] == 'S') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    static int countXMasShapes(char[][] grid) {
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+
+        // For each center
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (checkXMasShape(grid, i, j)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder inputStringBuilder = new StringBuilder();
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (line.equals("done")) break; // done to signify end of input
+            inputStringBuilder.append(line).append("\n");
+        }
+
+        String[] lines = inputStringBuilder.toString().split("\n");
+        int rows = lines.length;
+        int cols = lines[0].length();
+
+        char[][] charGrid = new char[rows][cols];
+
+        // put input into char array form
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                charGrid[i][j] = lines[i].charAt(j);
+            }
+        }
+
+        int count = countXMasShapes(charGrid);
+
+        System.out.println(count);
+    }
+}
